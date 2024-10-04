19d836ec383caac182e31e75cc07ed060e4df3f7
EGOR <a666@iMac-666.Dlink>
2024-10-04 18:11:52 +0300

    dz part3

--- letsgo.c
+++ letsgo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void){
-printf(" Let's\n");
-printf(" go\n");
-printf(" to walk\n");
+printf("    Let's\n");
+printf("        go\n");
+printf("            to walk\n");
 return 0;
 }
